4624625cb5286f877da2def533e87eb41c810f70
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-09-08 11:20:03 -0700

    another set

---
+++ LC252_MeetingRooms/MeetingRoomsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// lc 253 is here as well
+namespace LC252_MeetingRooms
+{
+    [TestClass]
+    public class MeetingRoomsTests
+    {
+        [TestMethod]
+        public void GivenOverlappingInputArray_CanAttend_ShouldReturnFalse()
+        {
+            var meetings = new List<Tuple<int, int>> { new Tuple<int, int>(3, 4), new Tuple<int, int>(1, 3), new Tuple<int, int>(1, 2)};
+
+            bool result = CanAttend(meetings);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GivenNonOverlappingInputArray_CanAttend_ShouldReturnTrue()
+        {
+            var meetings = new List<Tuple<int, int>> { new Tuple<int, int>(3, 4), new Tuple<int, int>(1, 2), new Tuple<int, int>(2, 3) };
+
+            bool result = CanAttend(meetings);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GivenOverlappingInputArray_GetNumRooms_ShouldReturnNumberOfRoomsRequired()
+        {
+            var meetings = new List<Tuple<int, int>> { new Tuple<int, int>(3, 4), new Tuple<int, int>(1, 3), new Tuple<int, int>(1, 2) };
+
+            int result = GetNumberOfRequiredRooms(meetings);
+
+            Assert.IsTrue(result == 2);
+        }
+
+        private int GetNumberOfRequiredRooms(List<Tuple<int, int>> meetings)
+        {
+            if(meetings.Count == 0)
+            {
+                return 0;
+            }
+
+            meetings.Sort();
+            var result = 1;
+     
+            for (int i = 1; i < meetings.Count; i++)
+            {
+                if (meetings[i].Item1 < meetings[i - 1].Item2)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        private bool CanAttend(List<Tuple<int, int>> meetings)
+        {
+            // param validation
+            if (meetings.Count < 2)
+            {
+                return true;
+            }
+
+            meetings.Sort();
+
+            for (int i = 1; i < meetings.Count; i++)
+            {
+                if (meetings[i].Item1 < meetings[i - 1].Item2)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LC260_SingleNumber/SingleNumberTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LC260_SingleNumber
+{
+    [TestClass]
+    public class SingleNumberTests
+    {
+        [TestMethod]
+        public void GivenValidInputArray_FindSingleNumbersHas_ShouldReturnTwoSingleNumbers()
+        {
+            var input = new int[] { 1, 2, 1, 3, 2, 5 };
+
+            int[] result = FindSingleNumberUsingHash(input);
+
+            Assert.IsTrue(result.SequenceEqual(new int[] { 3, 5}) || result.SequenceEqual(new int[] { 5, 3}));
+        }
+
+        [TestMethod]
+        public void GivenValidInputArray_FindSingleNumbersConstantSpace_ShouldReturnTwoSingleNumbers()
+        {
+            var input = new int[] { 1, 2, 1, 3, 2, 5 };
+
+            int[] result = FindSingleNumberConstantSpace(input);
+
+            Assert.IsTrue(result.SequenceEqual(new int[] { 3, 5 }) || result.SequenceEqual(new int[] { 5, 3 }));
+        }
+
+        private int[] FindSingleNumberConstantSpace(int[] input)
+        {
+            int AXORB = 0;
+            foreach (var item in input)
+            {
+                AXORB ^= item;
+            }
+
+            // pick one bit as flag
+            int bitFlag = (AXORB & (~(AXORB - 1)));
+            int[] res = new int[2];
+            foreach (int item in input)
+            {
+                if ((item & bitFlag) == 0)
+                {
+                    res[0] ^= item;
+                }
+                else
+                {
+                    res[1] ^= item;
+                }
+            }
+            return res;
+        }
+
+        private int[] FindSingleNumberUsingHash(int[] input)
+        {
+            var hashSet = new HashSet<int>();
+            foreach (var item in input)
+            {
+                if (hashSet.Contains(item))
+                {
+                    hashSet.Remove(item);
+                }
+                else
+                {
+                    hashSet.Add(item);
+                }
+            }
+
+            var array = new int[2];
+            hashSet.CopyTo(array);
+
+            return array;
+        }
+    }
+}
