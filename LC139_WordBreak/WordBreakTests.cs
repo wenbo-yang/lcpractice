2690193ae798428ee44ed54a77d81191ab6e9085
Wenbo.Yang <wenbo.yang@premera.com>
2019-09-12 14:08:04 -0700

    lc139 add new implementation

--- LC139_WordBreak/WordBreakTests.cs
+++ LC139_WordBreak/WordBreakTests.cs
@@ -19,29 +19,129 @@ namespace LC139_WordBreak
 
             Assert.IsTrue(result);
         }
+
         [TestMethod]
-        public void GivenDictionaryAndInCorrectString_WordBreak_ShouldReturnTrue()
+        public void GivenDictionaryAndIncorrectString_WordBreak_ShouldReturnFalse()
         {
             var set = new HashSet<string>() { "cats", "cat", "sand", "dog", "and" };
-            var target = "catsanddog";
+            var target = "catsandog";
 
             bool result = WorkBreak(target, set);
 
             Assert.IsFalse(result);
         }
 
+        [TestMethod]
+        public void GivenDictionaryAndCorrectString_WordBreakSearch_ShouldReturnTrue()
+        {
+            var set = new HashSet<string>() { "leet", "code" };
+            var target = "leetcode";
+
+            bool result = WorkBreakSearch(target, set);
+
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void GivenDictionaryAndIncorrectString_WordBreakSearch_ShouldReturnFalse()
+        {
+            var set = new HashSet<string>() { "cats", "cat", "sand", "dog", "and" };
+            var target = "catsandog";
+
+            bool result = WorkBreakSearch(target, set);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GivenDictionaryAndCorrectString_WordBreakDPSingle_ShouldReturnTrue()
+        {
+            var set = new HashSet<string>() { "leet", "code" };
+            var target = "leetcode";
+
+            bool result = WordBreakDPSingle(target, set);
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenDictionaryAndIncorrectString_WordBreakDPSingle_ShouldReturnFalse()
+        {
+            var set = new HashSet<string>() { "cats", "cat", "sand", "dog", "and" };
+            var target = "catsandog";
+
+            bool result = WordBreakDPSingle(target, set);
+
+            Assert.IsFalse(result);
+        }
+
+        private bool WordBreakDPSingle(string target, HashSet<string> set)
+        {
+            // generate dp
+
+            var dp = new bool[target.Length];
+
+            // initialize dp
+            var startIndex = -1;
+            for (int i = 1; i < target.Length; i++)
+            {
+                dp[i] = IsSubstringInDictionary(target, 0, i, set);
+                if (dp[i] && startIndex == -1)
+                {
+                    startIndex = i;
+                }
+            }
+
+            for (int i = startIndex; i < target.Length; i++)
+            {
+                for (int j = i; j < target.Length; j++)
+                {
+                    dp[j] = (dp[j] || dp[i - 1] && IsSubstringInDictionary(target, i, j, set));
+                }
+            }
+
+            return dp[dp.Length - 1];
+        }
+
+
+
+        private bool WorkBreakSearch(string target, HashSet<string> set)
+        {
+            return WorkBreakSearchHelper(target, 0, set);
+        }
+
+        private bool WorkBreakSearchHelper(string target, int startIndex, HashSet<string> set)
+        {
+            if (startIndex >= target.Length)
+            {
+                return true;
+            }
+
+            for (int i = startIndex + 1; i < target.Length; i++)
+            {
+                if (IsSubstringInDictionary(target, startIndex, i, set))
+                {
+                    return WorkBreakSearchHelper(target, i + 1, set);
+                }
+            }
+
+            return false;
+        }
 
         private bool WorkBreak(string target, HashSet<string> set)
         {
             var dp = GenerateDP(target.Length);
-            InitializeDP(dp, target, set);
+            var columnMem = InitializeDP(dp, target, set);
 
             for (int i = 1; i < target.Length; i++)
             {
                 for (int j = i; j < target.Length; j++)
                 {
-                    var l = j - i + 1;
-                    dp[i][j] = (j - l >= 0 && i - l >=0 && dp[i - l][j - l]) && IsSubstringInDictionary(target, i, j, set);
+                    dp[i][j] = columnMem.Contains(i - 1) && IsSubstringInDictionary(target, i, j, set);
+                    if (dp[i][j])
+                    {
+                        columnMem.Add(j);
+                    }
                 }
             }
 
@@ -70,12 +170,20 @@ namespace LC139_WordBreak
             return result;
         }
 
-        private void InitializeDP(bool[][] dp, string target, HashSet<string> set)
+        private HashSet<int> InitializeDP(bool[][] dp, string target, HashSet<string> set)
         {
+            var columnMem = new HashSet<int>();
             for (int i = 0; i < target.Length; i++)
             {
                 dp[0][i] = IsSubstringInDictionary(target, 0, i, set);
+
+                if (dp[0][i])
+                {
+                    columnMem.Add(i);
+                }
             }
+
+            return columnMem;
         }
 
         // could use sparse matrix to reduce storageh
