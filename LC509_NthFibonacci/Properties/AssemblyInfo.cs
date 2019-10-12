a11516ac184aa343f400938aaba59233dcc2fd6c
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-10-12 14:31:03 -0700

    add nth fib

---
+++ LC509_NthFibonacci/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC509_NthFibonacci")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC509_NthFibonacci")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0acac60a-a613-4946-a3a9-65756fdc74a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC509_NthFibonacci/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC509_NthFibonacci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // fn = (phi)^(n+1)/sqrt(5) - (phi')^(n+1)/sqrt(5);
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
