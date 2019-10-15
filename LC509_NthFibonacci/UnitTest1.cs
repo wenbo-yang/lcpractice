eb89f36c2c6befa7ae982a6cf54339031f8c9c3d
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-10-14 19:34:15 -0700

    test fib

--- LC509_NthFibonacci/UnitTest1.cs
+++ LC509_NthFibonacci/UnitTest1.cs
@@ -11,6 +11,43 @@ namespace LC509_NthFibonacci
         [TestMethod]
         public void TestMethod1()
         {
+            for (int i = 1; i < 100; i++)
+            {
+
+                if (FibNth(i) < FibNth(i - 1))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+        private ulong FibNth(int n)
+        {
+            ulong n_prev1 = 1;
+            ulong n_prev2 = 0;
+
+            if (n == 0)
+            {
+                return n_prev2;
+            }
+
+            if (n == 1)
+            {
+                return n_prev1;
+            }
+
+            ulong n_curr = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                n_curr = n_prev1 + n_prev2;
+
+                n_prev2 = n_prev1;
+                n_prev1 = n_curr;
+            }
+
+            return n_curr;
         }
     }
 }
