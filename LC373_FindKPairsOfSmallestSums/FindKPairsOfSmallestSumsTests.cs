802289a8d986e55c25bbbd6d00382e0afe1dd7a5
Wenbo.Yang <wenbo.yang@premera.com>
2019-10-18 16:40:59 -0700

    add another question

---
+++ LC373_FindKPairsOfSmallestSums/FindKPairsOfSmallestSumsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC373_FindKPairsOfSmallestSums
+{
+    [TestClass]
+    public class FindKPairsOfSmallestSumsTests
+    {
+        [TestMethod]
+        // swap two array elements such that on each index array1[i] is smaller than array2[i]
+        // then calculate sum based on diagnally until k
+        public void GivenTwoSortedArrays_FindKPairsOfSmallestSums_ShouldReturnCorrectResult()
+        {
+            var array1 = new int[] { 1, 7, 11 };
+            var array2 = new int[] { 2, 4, 6 };
+
+            var k = 3;
+
+            var result = FindKPairsOfSmallestSums(array1, array2, k);
+
+        }
+
+        private List<Tuple<int, int>> FindKPairsOfSmallestSums(int[] array1, int[] array2, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LC373_FindKPairsOfSmallestSums/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC373_FindKPairsOfSmallestSums")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC373_FindKPairsOfSmallestSums")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0f305ef-94b1-4638-a4c8-8634089dd053")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
