a31f3ba427445ce30641880eabf310d4b71811f2
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-10-31 20:26:05 -0700

    lc532 dp

---
+++ LC532_ContinuousSubarraySum/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC532_ContinuousSubarraySum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC532_ContinuousSubarraySum")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8ecdd51-ec81-4125-a858-c837228e7afb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC532_ContinuousSubarraySum/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC532_ContinuousSubarraySum
+{
+    [TestClass]
+    public class ContinuousSubarraySumTests
+    {
+        [TestMethod]
+        public void GivenValidArray_DoesSubArrayExist_ShouldReturnTrue()
+        {
+            var array = new int[] { 23, 2, 4, 6, 7 };
+            var k = 6;
+
+            var result = DoesSubArrayExist(array, k);
+
+            Assert.IsTrue(result);
+        }
+
+        private bool DoesSubArrayExist(int[] array, int k)
+        {
+            // param validation // 
+
+            var sumArray = new int[array.Length + 1];
+
+            sumArray[0] = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                sumArray[i] = sumArray[i - 1] + array[i - 1];
+            }
+
+            for (int i = 0; i < sumArray.Length; i++)
+            {
+                for (int j = i + 2; j < sumArray.Length; j++)
+                {
+                    var sum = sumArray[j] - sumArray[i];
+
+                    if (sum % k == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
