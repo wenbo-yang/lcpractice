8fec32707deb1f8888024d90dc1efaabbb5a306f
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-11-06 19:19:18 -0800

    add 766

---
+++ LC766_ToeplitzMatrix/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC766_ToeplitzMatrix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC766_ToeplitzMatrix")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ec7285d-4db1-434a-9d43-879986219535")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC766_ToeplitzMatrix/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC766_ToeplitzMatrix
+{
+    [TestClass]
+    public class ToeplitzMatrixTests
+    {
+        [TestMethod]
+        public void GivenValidToeplitzMatrix_IsToeplitzMatrix_ShouldReturnTrue()
+        {
+            var input = new int[][] { new int[] { 1, 2, 3, 4 },
+                                      new int[] { 5, 1, 2, 3 },
+                                      new int[] { 9, 5, 1, 2 } };
+
+            var result = IsToeplitzMatrix(input);
+
+            Assert.IsTrue(result);
+        }
+
+        private bool IsToeplitzMatrix(int[][] input)
+        {
+            // param validation
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                for (int j = 1; j < input[0].Length; j++)
+                {
+                    var result = input[i][j] == input[i - 1][j - 1];
+                    if (!result)
+                    {
+                        return false;                    
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
