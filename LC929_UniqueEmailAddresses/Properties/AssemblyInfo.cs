f2c615bd2f45cfcb1c7ebc91d7678d51f76a0397
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-11-07 22:01:19 -0800

    another question

---
+++ LC929_UniqueEmailAddresses/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC929_UniqueEmailAddresses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC929_UniqueEmailAddresses")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b3cbac7-4ee8-4970-aa60-75d34e3b03a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC929_UniqueEmailAddresses/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC929_UniqueEmailAddresses
+{
+    [TestClass]
+    public class UniqueEmailAddressesTests
+    {
+        [TestMethod]
+        public void GivenListOfEmailAddresses_FindUniqueEmailAddresses_ShouldReturnCorrectNumber()
+        {
+            var input = new string[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" };
+
+            var result = FindUniqueEmailAddresses(input);
+
+            Assert.IsTrue(result == 2);
+        }
+
+        private int FindUniqueEmailAddresses(string[] input)
+        {
+
+            var hashSet = new HashSet<string>();
+            foreach (var address in input)
+            {
+                var list = new List<char>();
+                var index = 0;
+                while (index < address.Length)
+                {
+                    if (address[index] == '+')
+                    {
+                        index = address.IndexOf('@', index);
+                        if (index == -1)
+                        {
+                            break;
+                        }
+
+                        continue;
+                    }
+
+                    if (address[index] == '.')
+                    {
+                        index++;
+                        continue;
+                    }
+
+                    if (address[index] == '@')
+                    {
+                        list.AddRange(address.Substring(index, address.Length - index));
+                        break;
+                    }
+
+                    list.Add(address[index]);
+                    index++;
+                }
+
+                if (list.Count != 0)
+                {
+                    hashSet.Add(new string(list.ToArray()));
+                }
+            }
+
+            return hashSet.Count;
+        }
+    }
+}
