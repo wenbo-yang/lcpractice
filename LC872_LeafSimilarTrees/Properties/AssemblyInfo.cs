14115cb16d9284bfb81e274457049394ab40dbbc
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-11-09 12:28:48 -0800

    wenbo, lc 872

---
+++ LC872_LeafSimilarTrees/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC872_LeafSimilarTrees")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC872_LeafSimilarTrees")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16a9f10e-ca98-44c8-924c-fd6bfb3d6735")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC872_LeafSimilarTrees/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommonUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC872_LeafSimilarTrees
+{
+//   1
+//  2 3
+// 4 5 6 7
+
+//    1
+//   2 7
+//  3 6
+// 4 3
+//    5
+
+    [TestClass]
+    public class LeafSimiarTreesTests
+    {
+        [TestMethod]
+        public void GivenTwoSimilarTrees_AreLeavesSimiliar_ShouldReturnTrue()
+        {
+            var root1 = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", "2", "3", "4", "5", "6", "7" });
+            var root2 = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", "2", "7", "3", "6", null, null, "4", "3", null, null, null, null, null, "5" });
+
+            var result = AreLeavesSimiliar(root1, root2);
+
+            Assert.IsTrue(result);
+        }
+
+        private bool AreLeavesSimiliar(BinaryTreeNode root1, BinaryTreeNode root2)
+        {
+            var leafList1 = new List<int>();
+            var leafList2 = new List<int>();
+
+            GetLeafList(root1, leafList1);
+            GetLeafList(root2, leafList2);
+
+            return leafList1.Count == leafList2.Count && leafList1.SequenceEqual(leafList2);
+        }
+
+        private void GetLeafList(BinaryTreeNode root, List<int> leafList)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            GetLeafList(root.Left, leafList);
+            GetLeafList(root.Right, leafList);
+
+            if (root.Left == null && root.Right == null)
+            {
+                leafList.Add(root.Value);
+            }
+        }
+    }
+}
