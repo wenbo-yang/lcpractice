625e4f5855ebe4378191400b92a4596f93212c82
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-11-21 10:58:55 -0800

    wenbo add template

--- LC80_RemoveDuplicatesFromSortedArray/UnitTest1.cs
+++ LC80_RemoveDuplicatesFromSortedArray/UnitTest1.cs
@@ -4,11 +4,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LC80_RemoveDuplicatesFromSortedArray
 {
     [TestClass]
-    public class UnitTest1
+    public class RemoveDuplicatesFromSortedArrayTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenSortedArrayWithDuplicates_RemoveDuplicates_ShouldReturnArrayWith()
         {
+            var input = new int[] { 0, 0,1,1,1,2,2,3,3,4 };
+
+            var length = RemoveDuplicates(input);
+
+            Assert.IsTrue(length == 5);
+            Assert.IsTrue(input[0] == 0 && input[1] == 1 && input[2] == 2 && input[3] == 3 && input[4] == 4);
+        }
+
+        private int RemoveDuplicates(int[] input)
+        {
+
+            var i = 1;
+            var j = 0;
+
+            while (i < input.Length)
+            {
+                if (input[i] != input[j])
+                {
+                    j++;
+                    input[j] = input[i];
+                }
+
+                i++;
+            }
+
+            return j + 1;
         }
     }
 }
