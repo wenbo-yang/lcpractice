cbb98b75a597442f491166d31b79d9de9329f732
Wenbo Yang <wenbo.yang@premera.com>
2019-11-26 16:04:14 -0800

    add another set

---
+++ LC659_SplitArrayIntoConsecutiveSequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC659_SplitArrayIntoConsecutiveSequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC659_SplitArrayIntoConsecutiveSequence")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("021982ff-c76d-4113-908f-5e00c5f8ecae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC659_SplitArrayIntoConsecutiveSequence/SplitArrayIntoConsecutiveSequenceTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC659_SplitArrayIntoConsecutiveSequence
+{
+    [TestClass]
+    public class SplitArrayIntoConsecutiveSequenceTests
+    {
+        [TestMethod]
+        public void GivenConsecutiveSequences_CanSplit_ShouldReturnTrue()
+        {
+            var input = new int[] { 1, 2, 3, 3, 4, 4, 5, 5 };
+
+            var result = CanSplit(input);
+
+            Assert.IsTrue(result);
+        }
+
+        private bool CanSplit(int[] input)
+        {
+            var frequency = new Dictionary<int, int>();
+            var lacks = new Dictionary<int, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!frequency.ContainsKey(input[i]))
+                {
+                    frequency.Add(input[i], 0);
+                    lacks.Add(input[i], 0);
+                }
+
+                frequency[input[i]]++;
+            }
+
+            frequency.Add(input.Last() + 1, 0);
+            frequency.Add(input.Last() + 2, 0);
+            lacks.Add(input.Last() + 1, 0);
+            lacks.Add(input.Last() + 2, 0);
+            lacks.Add(input.Last() + 3, 0);
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (frequency[input[i]] == 0)
+                {
+                    continue;
+                }
+
+                if (lacks[input[i]] > 0)
+                {
+                    lacks[input[i]]--;
+                    lacks[input[i] + 1]++;
+                }
+                else if (frequency[input[i] + 1] > 0 && frequency[input[i] + 2] > 0)
+                {
+                    frequency[input[i] + 2]--;
+                    frequency[input[i] + 1]--;
+                    lacks[input[i] + 3]++;
+                }
+                else
+                {
+                    return false;
+                }
+
+                frequency[input[i]]--;
+            }
+
+            return true;
+        }
+    }
+}
