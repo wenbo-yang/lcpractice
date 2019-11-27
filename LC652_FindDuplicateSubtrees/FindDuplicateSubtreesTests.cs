16d2b714a41b2807d5130a67cd2c5450faaf93a0
Wenbo Yang <wenbo.yang@premera.com>
2019-11-26 16:57:56 -0800

    find duplicated sub trees

---
+++ LC652_FindDuplicateSubtrees/FindDuplicateSubtreesTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using CommonUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC652_FindDuplicateSubtrees
+{
+    [TestClass]
+    public class FindDuplicateSubtreesTests
+    {
+        [TestMethod]
+        public void GivenOneTree_FindDuplicateSubTree_ShouldReturnDuplicatedSubtree()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] {"1", "2", "3", "5", null,"2", "4", null,null, "4"});
+            var duplicatedTree = FindDuplicateSubtree(root);
+
+            Assert.IsTrue(duplicatedTree.Count == 2);
+        }
+
+        private List<BinaryTreeNode> FindDuplicateSubtree(BinaryTreeNode root)
+        {
+            var uniqueTrees = new HashSet<string>();
+            var result = new List<BinaryTreeNode>();
+
+            SerializeAndFindResults(root, uniqueTrees, result);
+
+            return result;
+        }
+
+        private string SerializeAndFindResults(BinaryTreeNode root, HashSet<string> uniqueTrees, List<BinaryTreeNode> result)
+        {
+            if (root == null)
+            {
+                return "null";
+            }
+
+            var left = SerializeAndFindResults(root.Left, uniqueTrees, result);
+            var right = SerializeAndFindResults(root.Right, uniqueTrees, result);
+
+            var serializedTree = root.Value.ToString() + "," + left + "," + right;
+
+            if (uniqueTrees.Contains(serializedTree))
+            {
+                result.Add(root);
+            }
+
+            uniqueTrees.Add(serializedTree);
+
+            return serializedTree;
+        }
+    }
+}
---
+++ LC652_FindDuplicateSubtrees/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC652_FindDuplicateSubtrees")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC652_FindDuplicateSubtrees")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35fd6ee9-fb5e-40dc-88d2-7f6024371cae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
