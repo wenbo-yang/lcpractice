d1f13d02d5ea77c78b86027d8d82c582573e78d0
Wenbo Yang <wenbo.yang.84@gmail.com>
2019-12-01 00:14:51 -0800

    lc1269

--- LC1269_NumberOfWaysToStayAtTheSamePlace/NumberOfWaysToStayAtTheSamePlaceTests.cs
+++ LC1269_NumberOfWaysToStayAtTheSamePlace/NumberOfWaysToStayAtTheSamePlaceTests.cs
@@ -9,60 +9,53 @@ namespace LC1269_NumberOfWaysToStayAtTheSamePlace
         [TestMethod]
         public void GivenArrayLengthAndSteps_GetNumberOfWays_ShouldReturnCorrectNumberOfWays()
         {
-            var steps = 3; var arrLen = 2;
+            var steps = 4; var arrLen = 2;
 
             var result = GetNumberOfWays(steps, arrLen);
 
-            Assert.IsTrue(result == 4);
+            Assert.IsTrue(result == 8);
         }
 
         private int GetNumberOfWays(int steps, int arrLen)
         {
             var dp = GenerateDP(steps, arrLen);
 
-            var result = 0;
+            InitializeDP(dp);
+            CreateDP(dp, arrLen);
 
-            for (int i = 0; i < arrLen; i++)
+            return dp[steps][0];
+        }
+
+        private void CreateDP(int[][] dp, int arrLen)
+        {
+
+            for (int i = 1; i < dp.Length; i++)
             {
-                if (CanTravelToPositionAndBack(i, steps))
+                dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+                dp[i][arrLen - 1] = dp[i - 1][arrLen - 2] + dp[i - 1][arrLen - 1];
+
+                for (var j = i; j < dp[0].Length - 1; j++)
                 {
-                    result += GetNumberOfWaysToTravelToPositionAndBack(i, steps, dp);
-                }               
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j] + dp[i - 1][j + 1];
+                }
             }
-
-            return 0;
         }
 
-        private int GetNumberOfWaysToTravelToPositionAndBack(int position, int steps, int[][] dp)
+        private void InitializeDP(int[][] dp)
         {
-            return GetNumberOfWaysToTravelToPositionWithNSteps(position, position, dp) * GetNumberOfWaysToTravelToPositionWithNSteps(position, steps - position, dp);
-        }
-
-        private int GetNumberOfWaysToTravelToPositionWithNSteps(int position, int steps, int[][] dp)
-        {
-            throw new NotImplementedException();
+            dp[0][0] = 1;
         }
 
         private int[][] GenerateDP(int steps, int arrLen)
         {
-            var dp = new int[steps][];
+            var dp = new int[steps + 1][];
 
-            for (int i = 0; i < steps; i++)
+            for (int i = 0; i < steps + 1; i++)
             {
                 dp[i] = new int[arrLen];
             }
 
             return dp;
-        }
-
-        private bool CanTravelToPositionAndBack(int steps, int position)
-        {
-            if (steps == 0 && position == 0)
-            {
-                return true;
-            }
-
-            return steps > position * 2;
         }
     }
 }
