bd1ebc4bcc5da95de73fb381a2d18a1d51bc796d
Wenbo Yang <wenbo.yang@premera.com>
2020-02-13 15:50:00 -0800

    another question

---
+++ LC1301_DeepestLeaveSum/DeepestLeavesSumTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CommonUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC1301_DeepestLeavesSum
+{
+    [TestClass]
+    public class DeepestLeavesSumTests
+    {
+        [TestMethod]
+        public void GivenBinaryTree_DeepestLeavesSum_ShouldReturnCorrectSum()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", "2", "3", "4", "5", null, "6", "7", null, null, null, null, "8" });
+
+            var sum = DeepestLeavesSum(root);
+
+            Assert.IsTrue(sum == 15);
+        }
+
+        private int DeepestLeavesSum(BinaryTreeNode root)
+        {
+            (int depth, int value) result = (-1, int.MinValue);
+
+            TraverseTreePostOrder(root, 0, ref result);
+
+            return result.value;
+        }
+
+        private void TraverseTreePostOrder(BinaryTreeNode root, int currentDepth, ref (int depth, int value) result)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            TraverseTreePostOrder(root.Left, currentDepth + 1, ref result);
+            TraverseTreePostOrder(root.Right, currentDepth + 1, ref result);
+
+            if (currentDepth == result.depth)
+            {
+                result.value += root.Value;
+            }
+            else if (currentDepth > result.depth)
+            {
+                result.depth = currentDepth;
+                result.value = root.Value;
+            }
+        }
+    }
+}
---
+++ LC1301_DeepestLeaveSum/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC1301_DeepestLeaveSum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC1301_DeepestLeaveSum")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d237b171-2fda-4bb8-baa1-2d439c4ed4f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
