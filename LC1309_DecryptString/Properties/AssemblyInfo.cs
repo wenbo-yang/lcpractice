d5c281475267c02f322d4648de66cdf1ae3b6bdb
Wenbo Yang <wenbo.yang@premera.com>
2020-02-13 17:19:11 -0800

    another question

---
+++ LC1309_DecryptString/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC1309_DecryptString")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC1309_DecryptString")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d381841-2a2f-429f-8f12-d0bf0638962b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC1309_DecryptString/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC1309_DecryptString
+{
+    [TestClass]
+    public class DecryptStringTests
+    {
+        [TestMethod]
+        public void GivenString_DecryptString_ShouldReturnCorrectString()
+        {
+            var input = "110#11#12";
+
+            var result = DecryptString(input);
+
+            Assert.IsTrue(result == "ajkab");
+        }
+
+        private string DecryptString(string input)
+        {
+            var right = input.Length - 1;
+            var stack = new Stack<char>();
+            while (right >= 0)
+            {
+                char decryptedChar;
+                if (input[right] == '#')
+                {
+                    right = right - 2;
+
+                    var substring = input.Substring(right, 2);
+
+                    decryptedChar = DecryptHelper(substring);
+                }
+                else
+                {
+                    decryptedChar = DecryptHelper(input[right].ToString());    
+                }
+
+                stack.Push(decryptedChar);
+                right--;
+            }
+
+            return new string(stack.ToArray());
+        }
+
+        private char DecryptHelper(string substring)
+        {
+            return (char)('a' + Convert.ToInt32(substring) - 1);
+        }
+    }
+}
