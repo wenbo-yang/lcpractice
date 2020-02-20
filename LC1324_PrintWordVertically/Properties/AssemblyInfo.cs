a17db44fa8d9e850d6a43ff04a0bfd002cb1af29
Wenbo Yang <wenbo.yang@premera.com>
2020-02-20 15:47:36 -0800

    another question

---
+++ LC1324_PrintWordVertically/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC1324_PrintWordVertically")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC1324_PrintWordVertically")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89a0bef6-8b8d-473b-9425-685c34ea3e00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC1324_PrintWordVertically/VerticalPrintTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC1324_PrintWordVertically
+{
+    [TestClass]
+    public class VerticalPrintTests
+    {
+        [TestMethod]
+        public void GivenWordString_VerticalPrint_ShouldReturnCorrectArray()
+        {
+            var source = "HOW ARE YOU";
+
+            var result = VerticalPrint(source);
+
+            Assert.IsTrue(result[0] == "HAY");
+            Assert.IsTrue(result[1] == "ORO");
+            Assert.IsTrue(result[2] == "WEU");
+        }
+
+        private List<string> VerticalPrint(string s)
+        {
+            var result = new List<string>();
+
+            var array = s.Split(' ');
+            var maxLength = 0;
+            foreach(var word in array)
+            {
+                maxLength = Math.Max(maxLength, word.Length);
+            }
+
+            var mat = new char[maxLength][];
+            for (int i = 0; i < mat.Length; i++)
+            {
+                mat[i] = new char[array.Length];
+                for (int j = 0; j < array.Length; j++)
+                {
+                    mat[i][j] = ' ';
+
+                }
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    mat[j][i] = array[i][j]; 
+                }
+            }
+
+            foreach (var row in mat)
+            {
+                result.Add(new string(row).TrimEnd());
+            }
+
+            return result;
+        }
+    }
+}
