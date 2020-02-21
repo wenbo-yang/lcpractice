126e7a52dfb587f42b7f4f36f072cde63eab068f
Wenbo Yang <wenbo.yang@premera.com>
2020-02-20 16:24:58 -0800

    another question

---
+++ LC1325_DeleteLeavesWithAGivenValue/DeleteLeavesWithAGivenValueTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using CommonUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC1325_DeleteLeavesWithAGivenValue
+{
+    [TestClass]
+    public class DeleteLeavesWithAGivenValueTests
+    {
+        [TestMethod]
+        public void GivenBinaryTreeAndValue_DeleteLeavesWithGivenValueShouldDeleteTheLeaves()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", "2", "3", "2", null, "2", "4" });
+            var value = 2;
+            
+            root = DeleteLeavesWithGivenValue(root, value);
+            Assert.IsTrue(root.Left == null);
+        }
+
+        [TestMethod]
+        public void GivenAnotherBinaryTreeAndValue_DeleteLeavesWithGivenValueShouldDeleteTheLeaves()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", "1", "1"});
+            var value = 1;
+
+            root = DeleteLeavesWithGivenValue(root, value);
+            Assert.IsTrue(root == null);
+        }
+
+        private BinaryTreeNode DeleteLeavesWithGivenValue(BinaryTreeNode root, int value)
+        {
+            if(DeleteLeavesWithGivenValueHelper(root, value))
+            {
+                root = null;
+            }
+
+            return root;
+        }
+
+        private bool DeleteLeavesWithGivenValueHelper(BinaryTreeNode root, int value)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.Left != null && DeleteLeavesWithGivenValueHelper(root.Left, value))
+            {
+                root.Left = null;
+            }
+
+            if (root.Right != null && DeleteLeavesWithGivenValueHelper(root.Right, value))
+            {
+                root.Right = null;
+            }
+
+            if (root.Left == null && root.Right == null)
+            {
+                return root.Value == value;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LC1325_DeleteLeavesWithAGivenValue/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC1325_DeleteLeavesWithAGivenValue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC1325_DeleteLeavesWithAGivenValue")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec18380f-bf23-431c-8e7b-3a38c2c6250a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
