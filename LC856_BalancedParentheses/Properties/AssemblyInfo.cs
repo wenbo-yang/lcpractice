691fe0f4cdab8ffa5617b4e78444c5548485b383
Wenbo Yang <wenbo.yang.84@gmail.com>
2020-03-01 14:12:18 -0800

    merge

---
+++ LC856_BalancedParentheses/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC856_BalancedParentheses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC856_BalancedParentheses")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91b80be2-0185-43b5-900c-f3174604a47c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LC856_BalancedParentheses/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC856_BalancedParentheses
+{
+    [TestClass]
+    public class BalancedParenthesesScoreTests
+    {
+        [TestMethod]
+        public void GivenString_GetScoreOfParentheses_ShouldReturnCorrectScore()
+        {
+            var s = "(()(()))";
+            var score = GetScoreOfParenthese(s);
+
+            Assert.IsTrue(score == 6);
+        }
+
+        private int GetScoreOfParenthese(string s)
+        {
+            var openClosePair = new SortedDictionary<int, int>();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    openClosePair.Add(stack.Pop(), i);
+                }
+            }
+
+            return GetScoreHelper(s, 0, openClosePair) + GetScoreHelper(s, openClosePair[0] + 1, openClosePair);
+        }
+
+        private int GetScoreHelper(string s, int startIndex, SortedDictionary<int, int> openClosePair)
+        {
+            if (startIndex == s.Length)
+            {
+                return 0;
+            }
+
+
+        }
+    }
+}
