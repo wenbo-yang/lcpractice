ebebd2225a72b4edd50f8e8d4dc2b5cd58ce53db
Wenbo Yang <wenbo.yang.84@gmail.com>
2020-03-01 14:48:59 -0800

    another question

---
+++ LC1343_MaximumProductOfASplittedTree/MaximumProductOfASplittedTreeTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CommonUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LC1343_MaximumProductOfASplittedTree
+{
+    [TestClass]
+    public class MaximumProductOfASplittedTreeTests
+    {
+        [TestMethod]
+        public void GivenBinaryTree_MaximizeProduct_ShouldReturnMaximizedProduct()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", null, "2", "3", "4", null, null, "5", "6" });
+
+            var result = MaximizeProduct(root);
+
+            Assert.IsTrue(result == 90);
+        }
+
+        [TestMethod]
+        public void GivenAnotherBinaryTree_MaximizeProduct_ShouldReturnMaximizedProduct()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] { "2", "3", "9", "10", "7", "8", "6", "5", "4", "11", "1" });
+
+            var result = MaximizeProduct(root);
+
+            Assert.IsTrue(result == 1025);
+        }
+
+        [TestMethod]
+        public void GivenThirdBinaryTree_MaximizeProduct_ShouldReturnMaximizedProduct()
+        {
+            var root = BinaryTree.CreateBinaryTreeBFS(new string[] { "1", "1" });
+
+            var result = MaximizeProduct(root);
+
+            Assert.IsTrue(result == 1);
+        }
+
+        private int MaximizeProduct(BinaryTreeNode root)
+        {
+            SumTreePostOrder(root);
+            int currentMax = 0; 
+            GetProductPreorder(root, root.Value, ref currentMax);
+
+            return currentMax;
+        }
+
+        private void GetProductPreorder(BinaryTreeNode root, int globalSum, ref int currentMax)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            currentMax = Math.Max(currentMax, (globalSum - root.Value) * root.Value);
+            GetProductPreorder(root.Left, globalSum, ref currentMax);
+            GetProductPreorder(root.Right, globalSum, ref currentMax);
+        }
+
+        private int SumTreePostOrder(BinaryTreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            root.Value = SumTreePostOrder(root.Left) + SumTreePostOrder(root.Right) + root.Value;
+
+            return root.Value;
+        }
+    }
+}
+    
---
+++ LC1343_MaximumProductOfASplittedTree/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LC1343_MaximumProductOfASplittedTree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LC1343_MaximumProductOfASplittedTree")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18cb3043-be80-4e25-a1cf-a9550c2c8fc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
