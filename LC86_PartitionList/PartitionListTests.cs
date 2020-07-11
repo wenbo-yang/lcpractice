565fe6ba5c22d5871248114b620b4f9180ab3001
weyang <weyang@magicleap.com>
2020-07-11 15:33:24 -0700

    merge

--- LC86_PartitionList/PartitionListTests.cs
+++ LC86_PartitionList/PartitionListTests.cs
@@ -21,27 +21,63 @@ namespace LC86_PartitionList
                 result.Next.Next.Next.Next.Next.Value == 5);
         }
 
+        [TestMethod]
+        public void GivenAnotherLinkedList_PartitionList_ShouldReturnCorrectList()
+        {
+            var head = new LinkedListBuilder().ConstructFromArray(new int[] { 1, 4, 2, 3, 5, 2 }).Head;
+
+            var result = Partition(head, 3);
+
+            Assert.IsTrue(
+                result.Value == 1 && result.Next.Value == 2 && result.Next.Next.Value == 2 &&
+                result.Next.Next.Next.Value == 4 && result.Next.Next.Next.Next.Value == 3 &&
+                result.Next.Next.Next.Next.Next.Value == 5);
+        }
+
+        [TestMethod]
+        public void GivenThirdAnotherLinkedList_PartitionList_ShouldReturnCorrectList()
+        {
+            var head = new LinkedListBuilder().ConstructFromArray(new int[] { 2, 1 }).Head;
+
+            var result = Partition(head, 2);
+
+            Assert.IsTrue(
+                result.Value == 1 && result.Next.Value == 2);
+        }
+
+        [TestMethod]
+        public void Given312LinkedList_PartitionList_ShouldReturnCorrectList()
+        {
+            var head = new LinkedListBuilder().ConstructFromArray(new int[] { 3, 1, 2 }).Head;
+
+            var result = Partition(head, 3);
+
+            Assert.IsTrue(
+                result.Value == 1 && result.Next.Value == 2 && result.Next.Next.Value == 3);
+        }
+
         private ListNode Partition(ListNode head, int x)
         {
             var stack = new Stack<ListNode>();
             var current = head;
             var positionFound = false;
             ListNode prev = null;
+            ListNode targetSlot = null;
             while (current != null)
             {
-                if (current.Value == x)
+                if (current.Value >= x)
                 {
                     positionFound = true;
                 }
 
                 if (current.Value < x && !positionFound)
                 {
-                    stack.Push(current);
+                    targetSlot = current;
                 }
 
                 if (current.Value < x && positionFound)
                 {
-                    RemoveAndInsert(prev, current, head, stack);
+                    RemoveAndInsertAfterTargetSlot(ref targetSlot, ref prev, ref current, ref head);
                 }
 
                 prev = current;
@@ -51,22 +87,24 @@ namespace LC86_PartitionList
             return head;
         }
 
-        private void RemoveAndInsert(ListNode prev, ListNode current, ListNode head, Stack<ListNode> stack)
+        private void RemoveAndInsertAfterTargetSlot(ref ListNode targetSlot, ref ListNode prev, ref ListNode current, ref ListNode head)
         {
             prev.Next = current.Next;
 
-            if (stack.Count == 0)
+            if (targetSlot == null)
             {
                 current.Next = head;
-                stack.Push(head);
+                head = current;
+                targetSlot = head;
             }
             else
             {
-                current.Next = stack.Peek().Next;
-                stack.Peek().Next = current.Next;
-
-                stack.Push(current);
+                current.Next = targetSlot.Next;
+                targetSlot.Next = current;
+                targetSlot = current;
             }
+
+            current = prev;
         }
     }
 }
